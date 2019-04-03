d9cae36d76095dc4cac458350a6efd31fbb5e243
mursitaffandi <mursitaffandi@gmail.com>
2019-04-03 15:11:34 +0700

    setting connection and auth

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "mursitdb.database.windows.net";
+    $user = "mursit";
+    $pass = "1q2w3e4r!";
+    $db = "lab_db";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
